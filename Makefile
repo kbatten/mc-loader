714eff025161afa9eea9957aa1bb785259fc5db8
Keith Batten <kbatten@gmail.com>
2010-09-02 17:23:57 -0700

    add sasl support

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 all: main.c
-	gcc -o mc-loader main.c -lmemcached -Wl,-rpath,/usr/local/lib
+	gcc -o mc-loader main.c -Wl,-rpath,/usr/local/lib -lmemcached
+
+debug: main.c
+	gcc -o mc-loader main.c -Wl,-rpath,/usr/local/lib -lmemcached -O0 -gfull
 
 clean:
 	rm -f mc-loader
+	rm -rf mc-loader.dSYM
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <libmemcached/memcached.h>
 
-static int parse_host(char * hostport, char ** hostname, int * port) {
+static int parse_host(char *hostport, char **hostname, int *port) {
   char *ptr;
   *hostname = strdup(hostport);
   ptr = strchr(*hostname, ':');
@@ -16,6 +16,20 @@ static int parse_host(char * hostport, char ** hostname, int * port) {
   return 0;
 }
 
+static int parse_auth(char *auth, char **username, char **password) {
+  char *ptr;
+  *username = strdup(auth);
+  ptr = strchr(*username, ':');
+  if (ptr != NULL) {
+    *ptr = '\0';
+    *password = strdup(ptr + 1);
+  } else {
+    *password = strdup("");
+  }
+
+  return 0;
+}
+
 int main(int argc, char **argv) {
 
   char *hostname;
@@ -23,28 +37,52 @@ int main(int argc, char **argv) {
   char *filename;
   bool check = false;
   bool binary = false;
+  bool sasl = false;
+  char *sasl_username;
+  char *sasl_password;
   int i;
   
   if (argc < 3) {
-    printf("mc-loader <server>:<port> <keyset> [check] [binary]\n");
+    printf("mc-loader <server>:<port> <keyset> [check] [binary] [sasl username:password]\n");
     exit (1);
   }
 
   parse_host(argv[1], &hostname, &port);
   filename = strdup(argv[2]);
 
-  for (i=3; i < argc ; i++) {
-    if (strncmp("check",argv[i],6) == 0) {
+  for (i=3; i < argc; i++) {
+    if (strncmp("check", argv[i], 6) == 0) {
       check = true;
     }
-    else if (strncmp("binary",argv[i],7) == 0) {
+    else if (strncmp("binary", argv[i], 7) == 0) {
       binary = true;
+    }
+    else if (strncmp("sasl", argv[i], 4) == 0) {
+      if (argc < (i+2)) {
+	fprintf(stderr, "Missing SASL username:password\n");
+	exit(1);
+      }
+      sasl = true;
+      binary = true;
+      parse_auth(argv[i+1], &sasl_username, &sasl_password);
+      i = i + 1;
     }
   }
 
   memcached_st *memc = memcached_create(NULL);
   if (binary) {
     memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_BINARY_PROTOCOL, 1);
+  }
+  if (sasl) {
+    if (sasl_client_init(NULL) != SASL_OK) {
+      fprintf(stderr, "Failed to initialize sasl library!\n");
+      exit(1);
+    }
+
+    memc->sasl = malloc(sizeof(struct memcached_sasl_st));
+    memc->sasl->callbacks = NULL;
+    memc->sasl->is_allocated = false;
+    memcached_set_sasl_auth_data(memc, sasl_username, sasl_password);
   }
   memcached_server_add(memc, hostname, port);
 
@@ -64,13 +102,13 @@ int main(int argc, char **argv) {
   FILE *file = fopen(filename, "r");
   while (fgets(buffer,63,file) != NULL) {
     key = buffer;
-    ptr = strchr(key,' ');
+    ptr = strchr(key, ' ');
     if (ptr == NULL) {
       continue;
     }
     *ptr = '\0';
     data = ptr + 1;
-    ptr = strchr(data,'\n');
+    ptr = strchr(data, '\n');
     if (ptr != NULL) {
       *ptr = '\0';
     }
@@ -99,5 +137,10 @@ int main(int argc, char **argv) {
       }
     }
   }
+
+  if (sasl) {
+    memcached_destroy_sasl_auth_data(memc);
+  }
+
   return rval;
 }
